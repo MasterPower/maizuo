b09df85c4b0da94b06bbb8923e0239286a2d842d
lpz10 <li2012670869@163.com>
2018-02-26 17:48:51 +0800

    1.1.4

--- src/App.js
+++ src/App.js
@@ -1,16 +1,34 @@
 import React, { Component } from 'react';
 import './sass/public.css';
 import './sass/app.scss';
+import './iconfont/iconfont.css';
 class App extends Component {
+	constructor(props){
+		super(props);
+		this.state={
+			navList:["首页","影片","影院","商城","我的","卖座卡"]
+		}
+	}
   render() {
     return (
       <div className="App">
       	<header>
-      		<i></i>
-      		<a>卖座电影</a>
-      		<span>北京</span>
-      		<b></b>
+      		<i className="iconfont icon-category index_nav"></i>
+      		<a className="appName">卖座电影</a>
+      		<b className="user iconfont icon-account"></b>
+      		<span className="location iconfont">北京 &#xe6a6;</span>
       	</header>
+      	<nav>
+      		<ul>
+      			{
+      				this.state.navList.map((item,index)=>{
+      					return(
+      						<li className="" key={index}><a>{item}<span className="iconfont">&#xe6a7;</span></a></li>
+      					)
+      				})
+      			}
+      		</ul>
+      	</nav>
       </div>
     );
   };
